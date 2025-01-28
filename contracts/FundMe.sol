8553400381dd8a82d480d23a49b2eafeff06f0f1
Syber88 <ssmagwaza88@gmail.com>
2025-01-28 15:07:23 +0200

    created methog to retrieve price of etherium in usd

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 contract FundMe {
 
     uint256 public minimumUsd = 5;
@@ -11,4 +12,14 @@ contract FundMe {
     }
 
     // function withdraw ()
+    function getPrice() public view returns (uint256)  {
+        // address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int256 price,,,) = priceFeed.latestRoundData();
+        return uint256(price * 1e10);
+    }
+
+    function getConversionRate() public {
+
+    }
 }
