19faed77d3ce04f4de1be05e7b394966c9de9350
Syber88 <ssmagwaza88@gmail.com>
2025-01-28 15:37:00 +0200

    implemented minimum amount to send using conversion rate method

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.24;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 contract FundMe {
 
-    uint256 public minimumUsd = 5;
+    uint256 public minimumUsd = 5e18;
     function fund() public payable{
         
-        require(msg.value >= 1e18, "didn't send enough eth"); 
+        require(getConversionRate(msg.value) >= minimumUsd, "didn't send enough eth"); 
 
     }
 
